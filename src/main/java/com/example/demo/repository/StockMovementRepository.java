582c736e82a61d8080e858ed564c72d11e5bd701
Andy <hei.andy.100@gmail.com>
2026-07-08 20:44:13 +0200

    Revert "feat: ajouter gestion stock IN/OUT avec StockMovement, StockService, StockController"
    This reverts commit 78743d2b44650d549957604cf8236c54b1fc8b1d.

--- src/main/java/com/example/demo/repository/StockMovementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.StockMovement;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StockMovementRepository extends JpaRepository<StockMovement, UUID> {}
